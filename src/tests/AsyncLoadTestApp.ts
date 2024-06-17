46e2108283327789e27f27934475fdf63ec58799
yyq-404 <makelifemuchsimpler@gmail.com>
2024-06-17 16:01:40 +0800

    增加Http测试代码

---
+++ src/tests/AsyncLoadTestApp.ts
@@ -0,0 +1,44 @@
+import {HttpHelper} from '../net/HttpHelper';
+
+/**
+ * 异步加载测试应用
+ */
+export class AsyncLoadTestApp {
+    /** 需要从服务器加载的图像url列表 */
+    private _urls: string[] = ['data/uv.jpg', 'data/test.jpg', 'data/p1.jpg'];
+    
+    /**
+     * 串行加载图片
+     * @return {Promise<void>}
+     */
+    public async loadImagesSequenceAsync(): Promise<void> {
+        for (let i: number = 0; i < this._urls.length; i++) {
+            let image: HTMLImageElement = await HttpHelper.loadImageAsync(this._urls[i]);
+            console.log('loadImagesSequence : ', i, image);
+        }
+    }
+    
+    /**
+     * 并行加载图片
+     */
+    public loadImagesParallel(): void {
+        // 使用Promise.all方法，以并发方式加载所有image文件
+        let _promises: Promise<HTMLImageElement>[] = [];
+        for (let i: number = 0; i < this._urls.length; i++) {
+            _promises.push(HttpHelper.loadImageAsync(this._urls[i]));
+        }
+        Promise.all(_promises).then((images: HTMLImageElement[]) => {
+            for (let i: number = 0; i < images.length; i++)
+                console.log('loadImagesParallel : ', images[i]);
+        });
+    }
+    
+    /**
+     * 异步加载文本
+     * @return {Promise<void>}
+     */
+    public async loadTextFileAsync(): Promise<void> {
+        let str: string = await HttpHelper.loadTextFileAsync('data/test.txt');
+        console.log(str);
+    }
+}
