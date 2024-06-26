f0d308c9bb98222977f9999cbe9070c9ec83b813
badhumor <makelifemuchsimpler@gmail.com>
2024-06-27 01:27:42 +0800

    调整代码结构，增减坐标系统工具类

--- src/common/DrawHelper.ts
+++ src/common/DrawHelper.ts
@@ -39,25 +39,28 @@ export class DrawHelper {
      */
     public static drawBoundBox(builder: GLMeshBuilder, mat: Matrix4, mins: Vector3, maxs: Vector3, color: Vector4 = Vector4Adapter.red): void {
         // 使用LINE_LOOP绘制底面，注意顶点顺序，逆时针方向，根据右手螺旋定则可知，法线朝外
-        builder.begin(builder.webglContext.LINE_LOOP); // 使用的是LINE_LOOP图元绘制模式
+        // 使用的是LINE_LOOP图元绘制模式
+        builder.begin(builder.webglContext.LINE_LOOP);
         {
             builder.color(color.r, color.g, color.b).vertex(mins.x, mins.y, mins.z); // 2   - - -
             builder.color(color.r, color.g, color.b).vertex(mins.x, mins.y, maxs.z); // 0   - - +
             builder.color(color.r, color.g, color.b).vertex(maxs.x, mins.y, maxs.z); // 4   + - +
             builder.color(color.r, color.g, color.b).vertex(maxs.x, mins.y, mins.z); // 6   + - -
-            builder.end(mat);
         }
+        builder.end(mat);
         // 使用LINE_LOOP绘制顶面，注意顶点顺序，逆时针方向，根据右手螺旋定则可知，法线朝外
-        builder.begin(builder.webglContext.LINE_LOOP); // 使用的是LINE_LOOP图元绘制模式
+        // 使用的是LINE_LOOP图元绘制模式
+        builder.begin(builder.webglContext.LINE_LOOP);
         {
             builder.color(color.r, color.g, color.b).vertex(mins.x, maxs.y, mins.z); // 3   - + -
             builder.color(color.r, color.g, color.b).vertex(maxs.x, maxs.y, mins.z); // 7   + + -
             builder.color(color.r, color.g, color.b).vertex(maxs.x, maxs.y, maxs.z); // 5   + + +
             builder.color(color.r, color.g, color.b).vertex(mins.x, maxs.y, maxs.z); // 1   - + +
-            builder.end(mat);
         }
+        builder.end(mat);
         // 使用LINES绘制
-        builder.begin(builder.webglContext.LINES); // 使用的是LINES图元绘制模式
+        // 使用的是LINES图元绘制模式
+        builder.begin(builder.webglContext.LINES);
         {
             builder.color(color.r, color.g, color.b).vertex(mins.x, mins.y, mins.z); // 2   - - -
             builder.color(color.r, color.g, color.b).vertex(mins.x, maxs.y, mins.z); // 3   - + -
@@ -67,8 +70,8 @@ export class DrawHelper {
             builder.color(color.r, color.g, color.b).vertex(maxs.x, maxs.y, maxs.z); // 5   + + +
             builder.color(color.r, color.g, color.b).vertex(maxs.x, mins.y, mins.z); // 6   + - -
             builder.color(color.r, color.g, color.b).vertex(maxs.x, maxs.y, mins.z); // 7   + + -
-            builder.end(mat);
         }
+        builder.end(mat);
     }
 
     /**
@@ -86,10 +89,10 @@ export class DrawHelper {
      * @param builder
      * @param mat
      * @param halfLen
-     * @param tc 纹理坐标数组，该数组保存48个number类型，共6组纹理坐标，
+     * @param textureCoordinate 纹理坐标数组，该数组保存48个number类型，共6组纹理坐标，
      * 每组8个纹理坐标值，可以映射到立方体的某个面上，其顺序是前、右、后、左、上、下。
      */
-    public static drawTextureCubeBox(builder: GLMeshBuilder, mat: Matrix4, halfLen: number = 0.2, tc: number[] = [
+    public static drawTextureCubeBox(builder: GLMeshBuilder, mat: Matrix4, halfLen: number = 0.2, textureCoordinate: number[] = [
                                          ...[0, 0, 1, 0, 1, 1, 0, 1], // 前面
                                          ...[0, 0, 1, 0, 1, 1, 0, 1], // 右面
                                          ...[0, 0, 1, 0, 1, 1, 0, 1], // 后面
@@ -100,45 +103,45 @@ export class DrawHelper {
     ): void {
         // 前面
         builder.begin(builder.webglContext.TRIANGLE_FAN);
-        builder.texCoordinate(tc[0], tc[1]).vertex(-halfLen, -halfLen, halfLen); // 0   - - +
-        builder.texCoordinate(tc[2], tc[3]).vertex(halfLen, -halfLen, halfLen); // 4   + - +
-        builder.texCoordinate(tc[4], tc[5]).vertex(halfLen, halfLen, halfLen); // 5   + + +
-        builder.texCoordinate(tc[6], tc[7]).vertex(-halfLen, halfLen, halfLen); // 1   - + +
+        builder.texCoordinate(textureCoordinate[0], textureCoordinate[1]).vertex(-halfLen, -halfLen, halfLen); // 0   - - +
+        builder.texCoordinate(textureCoordinate[2], textureCoordinate[3]).vertex(halfLen, -halfLen, halfLen); // 4   + - +
+        builder.texCoordinate(textureCoordinate[4], textureCoordinate[5]).vertex(halfLen, halfLen, halfLen); // 5   + + +
+        builder.texCoordinate(textureCoordinate[6], textureCoordinate[7]).vertex(-halfLen, halfLen, halfLen); // 1   - + +
         builder.end(mat);
         // 右面
         builder.begin(builder.webglContext.TRIANGLE_FAN);
-        builder.texCoordinate(tc[8], tc[9]).vertex(halfLen, -halfLen, halfLen); // 4   + - +
-        builder.texCoordinate(tc[10], tc[11]).vertex(halfLen, -halfLen, -halfLen); // 6   + - -
-        builder.texCoordinate(tc[12], tc[13]).vertex(halfLen, halfLen, -halfLen); // 7   + + -
-        builder.texCoordinate(tc[14], tc[15]).vertex(halfLen, halfLen, halfLen); // 5   + + +
+        builder.texCoordinate(textureCoordinate[8], textureCoordinate[9]).vertex(halfLen, -halfLen, halfLen); // 4   + - +
+        builder.texCoordinate(textureCoordinate[10], textureCoordinate[11]).vertex(halfLen, -halfLen, -halfLen); // 6   + - -
+        builder.texCoordinate(textureCoordinate[12], textureCoordinate[13]).vertex(halfLen, halfLen, -halfLen); // 7   + + -
+        builder.texCoordinate(textureCoordinate[14], textureCoordinate[15]).vertex(halfLen, halfLen, halfLen); // 5   + + +
         builder.end(mat);
         // 后面
         builder.begin(builder.webglContext.TRIANGLE_FAN);
-        builder.texCoordinate(tc[16], tc[17]).vertex(halfLen, -halfLen, -halfLen); // 6   + - -
-        builder.texCoordinate(tc[18], tc[19]).vertex(-halfLen, -halfLen, -halfLen); // 2   - - -
-        builder.texCoordinate(tc[20], tc[21]).vertex(-halfLen, halfLen, -halfLen); // 3   - + -
-        builder.texCoordinate(tc[22], tc[23]).vertex(halfLen, halfLen, -halfLen); // 7   + + -
+        builder.texCoordinate(textureCoordinate[16], textureCoordinate[17]).vertex(halfLen, -halfLen, -halfLen); // 6   + - -
+        builder.texCoordinate(textureCoordinate[18], textureCoordinate[19]).vertex(-halfLen, -halfLen, -halfLen); // 2   - - -
+        builder.texCoordinate(textureCoordinate[20], textureCoordinate[21]).vertex(-halfLen, halfLen, -halfLen); // 3   - + -
+        builder.texCoordinate(textureCoordinate[22], textureCoordinate[23]).vertex(halfLen, halfLen, -halfLen); // 7   + + -
         builder.end(mat);
         // 左面
         builder.begin(builder.webglContext.TRIANGLE_FAN);
-        builder.texCoordinate(tc[24], tc[25]).vertex(-halfLen, -halfLen, -halfLen); // 2   - - -
-        builder.texCoordinate(tc[26], tc[27]).vertex(-halfLen, -halfLen, halfLen); // 0   - - +
-        builder.texCoordinate(tc[28], tc[29]).vertex(-halfLen, halfLen, halfLen); // 1   - + +
-        builder.texCoordinate(tc[30], tc[31]).vertex(-halfLen, halfLen, -halfLen); // 3   - + -
+        builder.texCoordinate(textureCoordinate[24], textureCoordinate[25]).vertex(-halfLen, -halfLen, -halfLen); // 2   - - -
+        builder.texCoordinate(textureCoordinate[26], textureCoordinate[27]).vertex(-halfLen, -halfLen, halfLen); // 0   - - +
+        builder.texCoordinate(textureCoordinate[28], textureCoordinate[29]).vertex(-halfLen, halfLen, halfLen); // 1   - + +
+        builder.texCoordinate(textureCoordinate[30], textureCoordinate[31]).vertex(-halfLen, halfLen, -halfLen); // 3   - + -
         builder.end(mat);
         // 上面
         builder.begin(builder.webglContext.TRIANGLE_FAN);
-        builder.texCoordinate(tc[32], tc[33]).vertex(-halfLen, halfLen, halfLen); // 1   - + +
-        builder.texCoordinate(tc[34], tc[35]).vertex(halfLen, halfLen, halfLen); // 5   + + +
-        builder.texCoordinate(tc[36], tc[37]).vertex(halfLen, halfLen, -halfLen); // 7   + + -
-        builder.texCoordinate(tc[38], tc[39]).vertex(-halfLen, halfLen, -halfLen); // 3   - + -
+        builder.texCoordinate(textureCoordinate[32], textureCoordinate[33]).vertex(-halfLen, halfLen, halfLen); // 1   - + +
+        builder.texCoordinate(textureCoordinate[34], textureCoordinate[35]).vertex(halfLen, halfLen, halfLen); // 5   + + +
+        builder.texCoordinate(textureCoordinate[36], textureCoordinate[37]).vertex(halfLen, halfLen, -halfLen); // 7   + + -
+        builder.texCoordinate(textureCoordinate[38], textureCoordinate[39]).vertex(-halfLen, halfLen, -halfLen); // 3   - + -
         builder.end(mat);
         // 下面
         builder.begin(builder.webglContext.TRIANGLE_FAN);
-        builder.texCoordinate(tc[40], tc[41]).vertex(-halfLen, -halfLen, halfLen); // 0   - - +
-        builder.texCoordinate(tc[42], tc[43]).vertex(-halfLen, -halfLen, -halfLen); // 2   - - -
-        builder.texCoordinate(tc[44], tc[45]).vertex(halfLen, -halfLen, -halfLen); // 6   + - -
-        builder.texCoordinate(tc[46], tc[47]).vertex(halfLen, -halfLen, halfLen); // 4   + - +
+        builder.texCoordinate(textureCoordinate[40], textureCoordinate[41]).vertex(-halfLen, -halfLen, halfLen); // 0   - - +
+        builder.texCoordinate(textureCoordinate[42], textureCoordinate[43]).vertex(-halfLen, -halfLen, -halfLen); // 2   - - -
+        builder.texCoordinate(textureCoordinate[44], textureCoordinate[45]).vertex(halfLen, -halfLen, -halfLen); // 6   + - -
+        builder.texCoordinate(textureCoordinate[46], textureCoordinate[47]).vertex(halfLen, -halfLen, halfLen); // 4   + - +
         builder.end(mat);
     }
 }
