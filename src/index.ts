171db89f38bb4d8bc4d1af1cfc8be46113cea73f
yyq-404 <makelifemuchsimpler@gmail.com>
2024-06-26 14:33:33 +0800

    修改应用标题，切换应用增加释放逻辑。

--- src/index.ts
+++ src/index.ts
@@ -3,51 +3,64 @@ import {RotatingCubeApplication} from './app/demo/RotationCubeApplication';
 import {BasicWebGLApplication} from './app/demo/BasicWebGLApplication';
 import {CoordinateSystemApplication} from './app/demo/CoordinateSystemApplication';
 
-/** 选择器控件 */
-let select: HTMLSelectElement = document.getElementById('select') as HTMLSelectElement;
 /** 获取用于获得webgl上下文对象的HTMLCanvasElement元素 */
 let canvas: HTMLCanvasElement | null = document.getElementById('webgl') as HTMLCanvasElement;
 
 /** 应用集合 */
 const apps = {
-    'chapter 3: RotatingCubeApplication': RotatingCubeApplication,
-    'chapter 5: BasicWebGLApplication': BasicWebGLApplication,
-    'chapter 6: CoordinateSystemApplication': CoordinateSystemApplication
+    'Demo-1: RotatingCubeApplication': RotatingCubeApplication,
+    'Demo-2: BasicWebGLApplication': BasicWebGLApplication,
+    'Demo-3: CoordinateSystemApplication': CoordinateSystemApplication
+};
+
+/** 当前正在运行的App */
+let currentApp: BaseApplication | null = null;
+/** 选择器控件 */
+let select: HTMLSelectElement = document.getElementById('select') as HTMLSelectElement;
+/** 构建选择项 */
+Object.keys(apps).forEach((key) => select.options.add(new Option(key, key)));
+/** 选择控件回调 */
+select.onchange = () => {
+    const appName = select.value as keyof typeof apps;
+    if (currentApp) {
+        currentApp.stop();
+        currentApp.dispose();
+    }
+    const app: BaseApplication = new apps[appName](canvas);
+    runAppAsync(app).then().catch((reason) => {
+        console.log(reason);
+    });
 };
 
 /** 调试信息 */
 const [fps, tris, verts]: Text[] = ['fps', 'tris', 'verts'].map(value => createText(value));
 
-// 实现Application中的frameCallback回调函数
-// 在回调函数中或去Application的FPS数据
-// 然后将其值设置到对应的Overlay的FPS文本节点上
+/**
+ * 在HTML span元素中创建Text类型节点
+ * @param id
+ */
+function createText(id: string): Text {
+    // 根据id获取对应的span元素
+    let span: HTMLSpanElement = document.getElementById(id) as HTMLSpanElement;
+    // 在span中创建Text类型的子节点，其文字初始化为空字符串
+    let text: Text = document.createTextNode('');
+    // 将Text节点作为span元素的儿子节点
+    span.appendChild(text);
+    return text;
+}
+
+/**
+ * 实现Application中的frameCallback回调函数
+ * 在回调函数中或去Application的FPS数据
+ * 然后将其值设置到对应的Overlay的FPS文本节点上
+ * @param {BaseApplication} app
+ */
 function frameCallback(app: BaseApplication): void {
     // 目前暂时只显示FPS
     fps.nodeValue = String(app.fps.toFixed(0));
     tris.nodeValue = '0';
     verts.nodeValue = '0';
 }
-
-// 在HTML span元素中创建Text类型节点
-function createText(id: string): Text {
-    // 根据id获取对应的span元素
-    let elem: HTMLSpanElement = document.getElementById(id) as HTMLSpanElement;
-    // 在span中创建Text类型的子节点，其文字初始化为空字符串
-    let text: Text = document.createTextNode('');
-    // 将Text节点作为span元素的儿子节点
-    elem.appendChild(text);
-    return text;
-}
-
-/** 构建选择项 */
-Object.keys(apps).forEach((key) => select.options.add(new Option(key, key)));
-
-/** 选择控件回调 */
-select.onchange = () => {
-    const appName = select.value as keyof typeof apps;
-    const app: BaseApplication = new apps[appName](canvas);
-    runAppAsync(app).then();
-};
 
 /**
  * 异步执行。
@@ -57,6 +70,7 @@ async function runAppAsync(app: BaseApplication | (new (canvas: HTMLCanvasElemen
     if (typeof app === 'function') {
         app = new app(canvas);
     }
+    currentApp = app;
     app.frameCallback = frameCallback;
     await app.runAsync();
 }
