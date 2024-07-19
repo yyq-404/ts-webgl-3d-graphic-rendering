2f75992aee6f7a324aadb67db1c71c9e903dd9a4
yyq-404 <makelifemuchsimpler@gmail.com>
2024-07-19 14:55:30 +0800

    增加Html工具类。

---
+++ src/app/demo/HtmlHelper.ts
@@ -0,0 +1,101 @@
+/**
+ * 滑动条属性
+ */
+export type HtmlRangeProps = {
+    /** 编号 */
+    id: string,
+    /** 名称 */
+    name: string,
+    /** 值 */
+    value: string,
+    /** 滑动回调 */
+    onChange: (event: Event) => void,
+    /** 最小值 */
+    min: string,
+    /** 最大值 */
+    max: string
+}
+
+/**
+ * 选择菜单选项属性
+ */
+export type  HtmlSelectOptionProps = {
+    /** 名称 */
+    name: string,
+    /** 值 */
+    value: string
+}
+
+/**
+ * 选择菜单属性
+ */
+export type HtmlSelectProps = {
+    /** 选项集合 */
+    options: HtmlSelectOptionProps[];
+    /** 选择回调 */
+    onChange: (event: Event) => void;
+    /** 值 */
+    value: string
+}
+
+/**
+ * Html工具类。
+ */
+export class HtmlHelper {
+    /**
+     * 创建滑动条。
+     * @return {HTMLInputElement}
+     * @private
+     * @param props
+     */
+    public static createRange(props: HtmlRangeProps): HTMLInputElement {
+        const {id, value, onChange, min, max} = props;
+        const range: HTMLInputElement = document.createElement('input');
+        range.type = 'range';
+        range.id = id;
+        range.value = value;
+        range.onchange = onChange;
+        range.min = min;
+        range.max = max;
+        return range;
+    }
+    
+    /**
+     * 创建选择菜单。
+     * @param {string} textContent
+     * @param props
+     * @private
+     */
+    public static createSelect(textContent: string, props: HtmlSelectProps) {
+        const {options, onChange, value} = props;
+        const parent = document.getElementById('controls');
+        const label = document.createElement('label');
+        label.textContent = textContent + ' ';
+        label.style.position = 'relative';
+        label.style.float = 'left';
+        const select = document.createElement('select');
+        options.forEach(option => select.options.add(new Option(option.name, option.value)));
+        select.onchange = onChange;
+        select.value = value;
+        label.appendChild(select);
+        parent.appendChild(label);
+        const br = document.createElement('br');
+        parent.appendChild(br);
+        const br2 = document.createElement('br');
+        parent.appendChild(br2);
+    }
+    
+    /**
+     * 创建居中标签。
+     * @param {string} textContent
+     * @return {HTMLElement}
+     */
+    public static createCenterLabel(textContent: string): HTMLElement {
+        const b = document.createElement('label');
+        b.textContent = textContent;
+        b.style.position = 'absolute';
+        b.style.left = '50%';
+        b.style.transform = 'translateX(-50%)';
+        return b;
+    }
+}
