88addc0b06c4a9c50247fb912ef30bdcb134aa99
yyq-404 <makelifemuchsimpler@gmail.com>
2024-07-30 11:07:27 +0800

    obj模型增加了uv坐标参数。

--- src/common/geometry/model/ModelOBJ.ts
+++ src/common/geometry/model/ModelOBJ.ts
@@ -1,6 +1,7 @@
 import {Geometry} from '../Geometry';
 import {VertexStructure} from '../VertexStructure';
 import {Vector3} from '../../math/vector/Vector3';
+import {Vector2} from '../../math/vector/Vector2';
 
 /**
  * OBJ模型定义。
@@ -8,16 +9,20 @@ import {Vector3} from '../../math/vector/Vector3';
 export class ModelOBJ extends Geometry {
     /** 法向量集合 */
     private readonly _normals: Vector3[] = [];
+    /** uv坐标 */
+    private readonly _uvs: Vector2[] = [];
     
     /**
      * 构造
      * @param {Vector3[]} points
      * @param {Vector3[]} normals
+     * @param uvs
      */
-    public constructor(points: Vector3[] = [], normals: Vector3[] = []) {
+    public constructor(points: Vector3[] = [], normals: Vector3[] = [], uvs: Vector2[] = []) {
         super();
         this._points = points;
         this._normals = normals;
+        this._uvs = uvs;
     }
     
     /**
@@ -28,6 +33,7 @@ export class ModelOBJ extends Geometry {
         const vertex: VertexStructure = new VertexStructure();
         vertex.positions = this._points;
         vertex.normals = this._normals;
+        vertex.uvs = this._uvs;
         return vertex;
     }
 }
