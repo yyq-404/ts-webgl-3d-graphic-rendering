7ddfaa0999498733c081b692f55497f4e074a832
yyq-404 <makelifemuchsimpler@gmail.com>
2024-07-30 11:12:30 +0800

    obj模型场景增加了对双面光照、纹理贴图的展示。

--- src/scene/webgl2/model/ModelOBJScene.ts
+++ src/scene/webgl2/model/ModelOBJScene.ts
@@ -8,6 +8,8 @@ import {ModelOBJHelper} from '../../ModelOBJHelper';
 import {ModelOBJ} from '../../../common/geometry/model/ModelOBJ';
 import {GLRenderHelper} from '../../../webgl/GLRenderHelper';
 import {LightController} from '../../LightController';
+import {HtmlHelper} from '../../HtmlHelper';
+import {HttpHelper} from '../../../net/HttpHelper';
 
 
 /**
@@ -18,14 +20,16 @@ export class ModelOBJScene extends WebGL2Scene {
     private _objModel: ModelOBJ;
     /** 光源控制器 */
     private _lightController: LightController = new LightController();
+    /** 渲染类型 */
+    private _type: string = 'color';
+    /** 模型纹理 */
+    private _texture: WebGLTexture = null;
     
     /**
      * 构造
      */
     public constructor() {
         super(true);
-        this.attributeBits = GLAttributeHelper.POSITION.BIT | GLAttributeHelper.NORMAL.BIT;
-        this.camera.z = 50;
         this.canvas.style.background = 'black';
         GLRenderHelper.setDefaultState(this.gl);
     }
@@ -36,8 +40,8 @@ export class ModelOBJScene extends WebGL2Scene {
      */
     public override get shaderUrls(): Map<string, string> {
         return new Map<string, string>([
-            ['bns.vert', `${SceneConstants.webgl2ShaderRoot}/model/light.vert`],
-            ['bns.frag', `${SceneConstants.webgl2ShaderRoot}/model/light.frag`]
+            ['bns.vert', `${SceneConstants.webgl2ShaderRoot}/model/${this._type}.vert`],
+            ['bns.frag', `${SceneConstants.webgl2ShaderRoot}/model/${this._type}.frag`]
         ]);
     }
     
@@ -46,11 +50,35 @@ export class ModelOBJScene extends WebGL2Scene {
      * @return {Promise<void>}
      */
     public override async runAsync(): Promise<void> {
-        await super.initAsync();
-        this._objModel = await ModelOBJHelper.loadAsync('res/model/ch.oxbj');
+        await this.initAsync();
+        if (this._type === 'color') {
+            this.attributeBits = GLAttributeHelper.POSITION.BIT;
+            this.camera.z = 50;
+            this._lightController.location = new Vector3([0, 0, 15]);
+            this._objModel = await ModelOBJHelper.loadAsync('res/model/teapot.obj');
+        } else if (this._type === 'light') {
+            this.attributeBits = GLAttributeHelper.POSITION.BIT | GLAttributeHelper.NORMAL.BIT;
+            this.camera.z = 50;
+            this._lightController.location = new Vector3([0, 0, 15]);
+            this._objModel = await ModelOBJHelper.loadAsync('res/model/teapot.obj', true);
+            this._lightController.createLocationControls();
+        } else if (this._type === 'light_double') {
+            this.attributeBits = GLAttributeHelper.POSITION.BIT | GLAttributeHelper.NORMAL.BIT;
+            this.camera.z = 50;
+            this.gl.disable(this.gl.CULL_FACE);
+            this._lightController.location = new Vector3([0, 0, 15]);
+            this._objModel = await ModelOBJHelper.loadAsync('res/model/teapot_o.obj');
+            this._lightController.createLocationControls();
+        } else if (this._type === 'texture') {
+            this.attributeBits = GLAttributeHelper.POSITION.BIT | GLAttributeHelper.NORMAL.BIT | GLAttributeHelper.TEX_COORDINATE_0.BIT;
+            this.camera.z = 200;
+            this._lightController.createLocationControls();
+            this._lightController.location = new Vector3([0, 0, 50]);
+            this._objModel = await ModelOBJHelper.loadAsync('res/model/teapot_t.obj');
+            this._texture = await this.loadNormalTextureAsync('res/image/ghxp.png');
+        }
+        this.createControls();
         this.createBuffers(this._objModel);
-        this._lightController.location = new Vector3([0, 0, 15]);
-        this._lightController.createLocationControls();
     }
     
     /**
@@ -75,13 +103,70 @@ export class ModelOBJScene extends WebGL2Scene {
         this.program.setMatrix4(GLShaderConstants.MVPMatrix, this.mvpMatrix());
         this.program.setMatrix4(GLShaderConstants.MMatrix, this.worldMatrixStack.worldMatrix());
         this.program.setVector3(GLShaderConstants.cameraPosition, this.camera.position);
-        // this.program.setVector3(GLShaderConstants.lightLocation, new Vector3([0, 0, 15]));
         this.program.setVector3(GLShaderConstants.lightLocation, this._lightController.location);
         buffers.forEach((buffer: WebGLBuffer, attribute: IGLAttribute) => {
             this.program.setVertexAttribute(attribute.NAME, buffer, attribute.COMPONENT);
         });
+        if (this._texture) {
+            this.gl.activeTexture(this.gl.TEXTURE0);
+            this.gl.bindTexture(this.gl.TEXTURE_2D, this._texture);
+        }
         this.gl.drawArrays(this.gl.TRIANGLES, 0, this._objModel.vertex.count);
         this.worldMatrixStack.popMatrix();
         this.program.unbind();
     }
+    
+    /**
+     * 加载纹理。
+     * @return {Promise<WebGLTexture>}
+     * @private
+     */
+    private async loadNormalTextureAsync(url: string): Promise<WebGLTexture> {
+        const img = await HttpHelper.loadImageAsync(url);
+        const texture = this.gl.createTexture();
+        //绑定纹理ID
+        this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+        //加载纹理进缓存
+        this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, img);
+        this.setTextureParameters();
+        //纹理加载成功后释放纹理图
+        this.gl.bindTexture(this.gl.TEXTURE_2D, null);
+        return texture;
+    }
+    
+    /**
+     * 设置纹理参数。
+     * @private
+     */
+    private setTextureParameters(): void {
+        //设置MAG采样方式
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+        //设置MIN采样方式
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
+        //设置S轴拉伸方式
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
+        //设置T轴拉伸方式
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT);
+    }
+    
+    /**
+     * 创建控制控件。
+     * @private
+     */
+    private createControls(): void {
+        const sizes = ['color', 'light', 'light_double', 'texture'];
+        HtmlHelper.createRadioGroup('type', '类型: ', sizes, this.onTypeChange);
+    }
+    
+    /**
+     * 纹理坐标尺寸更改
+     */
+    private onTypeChange = (event: Event): void => {
+        let element = event.target as HTMLInputElement;
+        if (element.checked) {
+            this._type = element.value;
+        }
+        this.clearControls();
+        this.runAsync.apply(this);
+    };
 }
